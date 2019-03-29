3df68bdb672a24e8f2919ce986fd69289cb1fa77
Gian Lazzarini <gianlazzarini@gmail.com>
2019-03-29 15:25:46 -0700

    Setup dotenv in the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 const app = express();
-const port = 8080; // default port to listen
+// const port = 8080; // default port to listen
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an enviroment variable
+const port = process.env.SERVER_PORT;
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
