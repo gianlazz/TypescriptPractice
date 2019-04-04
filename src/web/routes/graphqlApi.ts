42111948167c34c9593598459cadddb88c345338
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-04 14:03:46 -0700

    Setting up graphql crud on Guitar typeorm entity

--- src/web/routes/graphqlApi.ts
+++ src/web/routes/graphqlApi.ts
@@ -3,15 +3,15 @@ import express from "express";
 import graphqlHTTP from "express-graphql";
 import { buildASTSchema } from "graphql";
 import gql from "graphql-tag";
+import { Guitar } from "../../dal/entity/guitar";
 
 export const register = ( app: express.Application ) => {
+    // Authorization
+    const oidc = app.locals.oidc;
+
     app.use(cors());
 
     const schema = buildASTSchema(gql`
-        type Query {
-            hello: String
-        }
-
         type Guitar {
             id: Int!
             userId: String!
@@ -21,6 +21,11 @@ export const register = ( app: express.Application ) => {
             color: String
         }
 
+        type Query {
+            guitars: [Guitar!]!
+            hello: String
+        }
+
         type Mutation {
             createGuitar(userId: String!, brand: String!, model: String!, year: Int, color: String): Guitar!
             deleteGuitar(id: Int!): Boolean
@@ -28,7 +33,25 @@ export const register = ( app: express.Application ) => {
     `);
 
     const rootValue = {
-        hello: () => "hello world"
+        Mutation: {
+            createGuitar: async (guitar: Guitar) => {
+                Guitar.create(guitar);
+                await guitar.save();
+                return guitar;
+            },
+            deleteGuitar: async (id: number) => {
+                try {
+                    Guitar.delete(id);
+                    return true;
+                } catch (error) {
+                    return false;
+                }
+            }
+        },
+        Query: {
+            guitars: async (userId: string) => await Guitar.find({ userId }),
+            hello: () => "hello world"
+        }
     };
 
     app.use("/graphql", graphqlHTTP({ schema, rootValue}));
