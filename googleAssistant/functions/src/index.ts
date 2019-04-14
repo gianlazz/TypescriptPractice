5957c2f81adc3f1835afe0936b142c1a91544c28
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-13 17:20:20 -0700

    Setup basic google assistant hello world

--- googleAssistant/functions/src/index.ts
+++ googleAssistant/functions/src/index.ts
@@ -4,8 +4,29 @@ import { dialogflow, SimpleResponse, BasicCard, Button, Image } from 'actions-on
 const app = dialogflow({ debug: true });
 
 // Capture Intent
-app.intent('Get Latest Episode', (conv) => {
-    
+app.intent('Hello Lazztech', async (conv) => {
+    const data = 'Hello World';
+
+    // conv.ask();
+
+    conv.close(new SimpleResponse({
+        text: `${data}`,
+        speech: `${data}`
+    }));
+    conv.close(new BasicCard({
+        title: 'Lazztech Assistant',
+        image: new Image({
+            url: '',
+            alt: ''
+        }),
+        buttons: new Button({
+            title: 'lazz.tech',
+            url: 'http://lazz.tech/',
+        })
+    }))
 });
+
+// Export the Cloud Functions
+export const fulfillment = functions.https.onRequest(app);
 
 
