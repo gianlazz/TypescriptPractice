e011c6de6fb5db2fdc7ee37ba61fb40599392c20
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-15 15:08:48 -0700

    Setup simple face demo to build ts and copy html

--- Simple-Face-ApiJS-Demo/src/index.ts
+++ Simple-Face-ApiJS-Demo/src/index.ts
@@ -1,4 +1,4 @@
-import * as faceapi from 'face-api.js';
+import * as faceapi from "face-api.js";
 
 export class FaceDemo {
 
@@ -7,27 +7,27 @@ export class FaceDemo {
     }
 
     private async loadModels() {
-        await faceapi.loadSsdMobilenetv1Model('/models');
-        console.log('Loaded loadSsdMobilenetv1Model');
-    
-        await faceapi.loadTinyFaceDetectorModel('/models');
-        console.log('Loaded loadTinyFaceDetectorModel');
-    
-        await faceapi.loadMtcnnModel('/models');
-        console.log('Loaded loadMtcnnModel');
-    
-        await faceapi.loadFaceLandmarkModel('/models');
-        console.log('Loaded loadFaceLandmarkModel');
-    
-        await faceapi.loadFaceLandmarkTinyModel('/models');
-        console.log('Loaded loadFaceLandmarkTinyModel');
-    
-        await faceapi.loadFaceRecognitionModel('/models');
-        console.log('Loaded loadFaceRecognitionModel');
-    
-        await faceapi.loadFaceExpressionModel('/models');
-        console.log('Loaded loadFaceExpressionModel');
-    
+        await faceapi.loadSsdMobilenetv1Model("/models");
+        console.log("Loaded loadSsdMobilenetv1Model");
+
+        await faceapi.loadTinyFaceDetectorModel("/models");
+        console.log("Loaded loadTinyFaceDetectorModel");
+
+        await faceapi.loadMtcnnModel("/models");
+        console.log("Loaded loadMtcnnModel");
+
+        await faceapi.loadFaceLandmarkModel("/models");
+        console.log("Loaded loadFaceLandmarkModel");
+
+        await faceapi.loadFaceLandmarkTinyModel("/models");
+        console.log("Loaded loadFaceLandmarkTinyModel");
+
+        await faceapi.loadFaceRecognitionModel("/models");
+        console.log("Loaded loadFaceRecognitionModel");
+
+        await faceapi.loadFaceExpressionModel("/models");
+        console.log("Loaded loadFaceExpressionModel");
+
         console.log(faceapi.nets);
     }
 }
