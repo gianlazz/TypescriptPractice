13b337ba2c471c4dc0cd2dde51e4bc31eb4d7d63
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-15 14:53:46 -0700

    Setting up typescript for simple face demo

---
+++ Simple-Face-ApiJS-Demo/src/index.ts
@@ -0,0 +1,33 @@
+import * as faceapi from 'face-api.js';
+
+export class FaceDemo {
+
+    constructor() {
+        this.loadModels();
+    }
+
+    private async loadModels() {
+        await faceapi.loadSsdMobilenetv1Model('/models');
+        console.log('Loaded loadSsdMobilenetv1Model');
+    
+        await faceapi.loadTinyFaceDetectorModel('/models');
+        console.log('Loaded loadTinyFaceDetectorModel');
+    
+        await faceapi.loadMtcnnModel('/models');
+        console.log('Loaded loadMtcnnModel');
+    
+        await faceapi.loadFaceLandmarkModel('/models');
+        console.log('Loaded loadFaceLandmarkModel');
+    
+        await faceapi.loadFaceLandmarkTinyModel('/models');
+        console.log('Loaded loadFaceLandmarkTinyModel');
+    
+        await faceapi.loadFaceRecognitionModel('/models');
+        console.log('Loaded loadFaceRecognitionModel');
+    
+        await faceapi.loadFaceExpressionModel('/models');
+        console.log('Loaded loadFaceExpressionModel');
+    
+        console.log(faceapi.nets);
+    }
+}
