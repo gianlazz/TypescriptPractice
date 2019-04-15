268ec7782e2c0bea0f2b90d4d306cf4f51056203
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-15 11:28:15 -0700

    Deleted commented cors code from server

--- server/src/web/index.ts
+++ server/src/web/index.ts
@@ -14,17 +14,6 @@ app.use( express.json() );
 
 // Configure Express to allow Cross Origin Scripting so server and client can communicate during dev
 app.use(cors());
-// Manual CORS Headers
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-//     if ("OPTIONS" === req.method) {
-//         res.sendStatus(200);
-//     } else {
-//         console.log(`${req.ip} ${req.method} ${req.url}`);
-//     }
-// });
 
 // initialize configuration
 dotenv.config();
