1551b0d883caa289b64e61bcac28faf9957a8ad0
Gian Lazzarini <gianlazzarini@gmail.com>
2019-04-17 13:54:26 -0700

    working on graphqlApi

--- server/src/web/routes/graphqlApi.ts
+++ server/src/web/routes/graphqlApi.ts
@@ -12,8 +12,6 @@ export const register = ( app: express.Application ) => {
     const oidc = app.locals.oidc;
 
     const schema = buildASTSchema(gql`
-        scalar JSON
-
         type Guitar {
             id: Int
             userId: String
@@ -42,44 +40,61 @@ export const register = ( app: express.Application ) => {
         type Mutation {
             createGuitar(userId: String!, brand: String!, model: String!, year: Int, color: String): Guitar
             deleteGuitar(id: Int!): Boolean
-            registerPersonsFace(name: String, image: String, descriptor: String): Int
+            registerPersonsFace(PersonsFace): Int
         }
     `);
 
     const rootValue = {
         createGuitar: async (guitar: Guitar) => {
+            try {
             guitar = await Guitar.create(guitar);
             guitar = await guitar.save();
             return guitar;
+            } catch (error) {
+                console.error(error);
+            }
+
         },
         deleteGuitar: async (id: number) => {
             try {
                 Guitar.delete(id);
                 return true;
             } catch (error) {
-                console.log(error);
+                console.error(error);
                 return false;
             }
         },
         // guitars: async (userId: string) => {
         guitars: async () => {
+            try {
             //  const guitars = await Guitar.find({ userId });
             const guitars = await Guitar.find();
             console.log(guitars);
             return guitars;
+            } catch (error) {
+                console.error(error);
+            }
         },
         hello: () => "hello world",
         recognizedFaces: async () => {
+            try {
             const results = await PersonsFace.find();
             console.log(results);
             return results;
+            } catch (error) {
+                console.error(error);
+            }
         },
         registerPersonsFace: async (personsFace: PersonsFace) => {
-            console.log("registerPersonsFace mutation hit");
-            personsFace = await PersonsFace.create(personsFace);
-            personsFace = await personsFace.save();
-            console.log(`registerPersonsFace mutation finished ${personsFace.id}`);
-            return personsFace.id;
+            try {
+                console.log("registerPersonsFace mutation hit");
+                personsFace = await PersonsFace.create(personsFace);
+                personsFace = await personsFace.save();
+                console.log(`registerPersonsFace mutation finished ${personsFace.id}`);
+                return personsFace.id;
+            } catch (error) {
+                console.error(error);
+            }
         },
     };
 
