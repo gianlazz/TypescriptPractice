43d3fc9576b30442992c4a3b298fddf704f72b89
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-04-19 16:30:03 -0700

    work env variables for debug/release in client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -67,7 +67,7 @@ const appRoutes: Routes = [
 export class AppModule { 
   constructor(apollo: Apollo, httpLink: HttpLink) {
     apollo.create({
-      link: httpLink.create({ uri: 'https://lazztechvision.herokuapp.com:8080/graphql' }),
+      link: httpLink.create({ uri: 'https://lazztechvision.herokuapp.com/graphql' }),
       cache: new InMemoryCache()
     })
   }
