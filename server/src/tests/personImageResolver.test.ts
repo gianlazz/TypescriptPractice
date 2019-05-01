8ec83ea75f0350d9704096bcb911cd1524884bfc
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-05-01 11:42:53 -0700

    working on the integration tests

--- server/src/tests/personImageResolver.test.ts
+++ server/src/tests/personImageResolver.test.ts
@@ -11,7 +11,33 @@ afterAll(async () => {
     await conn.close();
 });
 
-describe("PersonImageResolver", async () => {
+describe("PersonImageResolver", () => {
+    it("newImage should process multiple images and recognize that it's the same image.", async () => {
+        // Arrange
+        const image = "https://fortunedotcom.files.wordpress.com/2018/07/gettyimages-961697338.jpg";
+        const mutation = `
+        mutation {
+          newImage(inputImage: {
+            image: "https://fortunedotcom.files.wordpress.com/2018/07/gettyimages-961697338.jpg"
+          })
+        }
+        `;
+        // Act
+        const response1 = await gCall({ source: mutation });
+        const response2 = await gCall({ source: mutation });
+        // Assert
+        expect(response1).toMatchObject({
+          data: {
+            newImage: 1
+          }
+        });
+        expect(response2).toMatchObject({
+          data: {
+            newImage: 2
+          }
+        });
+    });
+
     it("getAllPersons query should return Person[].", async () => {
         // Arrange
         const query = `
@@ -19,10 +45,11 @@ describe("PersonImageResolver", async () => {
           getAllPersons {
             id
             name
-            firstSeenDateTime
             images {
               id
-              image
+              personDescriptors {
+                id
+              }
             }
           }
         }
@@ -35,16 +62,23 @@ describe("PersonImageResolver", async () => {
             getAllPersons: [
               {
                 id: "1",
-                name: "gian",
-                firstSeenDateTime: "now",
+                name: null,
                 images: [
                   {
                     id: "1",
-                    image: "p2image1"
+                    personDescriptors: [
+                      {
+                        id: "1"
+                      }
+                    ]
                   },
                   {
                     id: "2",
-                    image: "p2image2"
+                    personDescriptors: [
+                      {
+                        id: "2"
+                      }
+                    ]
                   }
                 ]
               }
