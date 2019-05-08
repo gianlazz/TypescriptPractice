35e6626990ab12320219b1eb3e2f93211c90e5af
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-05-08 10:55:29 -0700

    Setup working login form in ionic with apollo graphql client

--- ionic/Face/src/app/app.module.ts
+++ ionic/Face/src/app/app.module.ts
@@ -9,13 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
   ],
   providers: [
     StatusBar,
@@ -24,4 +33,11 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { 
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:8080/graphql' }),
+      cache: new InMemoryCache()
+    })
+  }
+}
