bee135ade6a46045e416ea11b3e96a95e7c76eba
Gian Lazzarini <gian.lazzarini@base2s.com>
2019-05-15 13:05:22 -0700

    Got person renaming working in the ionic app Person page

--- ionic/src/app/person/person.page.ts
+++ ionic/src/app/person/person.page.ts
@@ -12,8 +12,12 @@ export class PersonPage implements OnInit {
 
   private id: number;
   private person: any;
+  private renaming: boolean;
+  private newName: string;
 
-  constructor(private route: ActivatedRoute, private apollo: Apollo) { }
+  constructor(private route: ActivatedRoute, private apollo: Apollo) {
+    this.renaming = false;
+  }
 
   ngOnInit() {
     this.id = parseInt(this.route.snapshot.paramMap.get('id'));
@@ -36,7 +40,31 @@ export class PersonPage implements OnInit {
       this.person = data['getPerson'];
       console.log(JSON.stringify(this.person));
       console.log(this.person[0].images[0].image);
-    });``
+    });
+  }
+
+  startRenaming() {
+    this.renaming = true;
+  }
+
+  stopRenaming() {
+    this.renaming = false;
+  }
+
+  async rename() {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation {
+        renamePerson(personId: ${this.id}, newName: "${this.newName}")
+      }
+      `
+    }).subscribe(({data}) => {
+      if (data.renamePerson === true) {
+        this.person.name = this.newName;
+      } else {
+        console.log("Rename failed.");
+      }
+    });
   }
 
 }
